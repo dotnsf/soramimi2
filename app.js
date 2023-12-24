002401e3b3dba831061d6ab4875667790b3ff4f1
K.Kimura <dotnsf@gmail.com>
2023-12-24 14:00:13 +0900

    changes for github pages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require( 'express' ),
     app = express();
 
-app.use( express.static( __dirname + '/public' ) );
+app.use( express.static( __dirname + '/docs' ) );
 
 
 var port = process.env.port || 8080;
